e5457134da0527a4d92ca0d218eb61604f38c207
HamJeeHyum <2075jh@gmail.com>
2023-03-29 23:22:21 +0900

    fix: API 설명 추가

--- src/main/java/greengrocery/controller/FruitController.java
+++ src/main/java/greengrocery/controller/FruitController.java
@@ -3,6 +3,7 @@ package greengrocery.controller;
 import greengrocery.domain.common.Message;
 import greengrocery.service.FruitService;
 import greengrocery.util.ResponseUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,18 +20,21 @@ public class FruitController {
 
 
     @GetMapping("token")
+    @ApiOperation(value="과일 토큰 발급", notes="과일 목록 조회 및 가격 조회 시 필요한 토큰을 발급한다.")
     public ResponseEntity<Message> getToken() {
         return ResponseUtil.success(fruitService.getToken());
     }
 
 
     @GetMapping("list")
+    @ApiOperation(value="과일 목록 조회", notes="과일 목록 조회를 한다. (Header에 token 필요)")
     public ResponseEntity<Message> getFruitList(@RequestHeader String token) {
         return ResponseUtil.success(fruitService.getFruitList(token));
     }
 
 
     @GetMapping("price")
+    @ApiOperation(value="과일 가격 조회", notes="조회하고자하느 과일명을 parma으로 보내 가격을 조회한다. (Header에 token 필요)")
     public ResponseEntity<Message> getFruitPrice(@RequestHeader String token, @RequestParam String name) {
         return ResponseUtil.success(fruitService.getFruitPrice(token, name));
 
--- src/main/java/greengrocery/controller/VegetableController.java
+++ src/main/java/greengrocery/controller/VegetableController.java
@@ -3,6 +3,7 @@ package greengrocery.controller;
 import greengrocery.domain.common.Message;
 import greengrocery.service.VegetableService;
 import greengrocery.util.ResponseUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,17 +20,20 @@ public class VegetableController {
 
 
     @GetMapping("token")
+    @ApiOperation(value = "채소 토큰 발급", notes = "채소 목록 조회 및 가격 조회 시 필요한 토큰을 발급한다.")
     public ResponseEntity<Message> getToken() {
         return ResponseUtil.success(vegetableService.getToken());
     }
 
     @GetMapping("list")
+    @ApiOperation(value = "채소 목록 조회", notes = "채소 목록 조회를 한다. (Header에 token 필요)")
     public ResponseEntity<Message> getVegetableList(@RequestHeader String token) {
         return ResponseUtil.success(vegetableService.getVegetableList(token));
     }
 
 
     @GetMapping("price")
+    @ApiOperation(value = "채소 가격 조회", notes = "조회하고자하느 채소명을 parma으로 보내 가격을 조회한다. (Header에 token 필요)")
     public ResponseEntity<Message> getVegetablePrice(@RequestHeader String token, @RequestParam String name) {
         return ResponseUtil.success(vegetableService.getVegetablePrice(token, name));
     }
